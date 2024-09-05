1b013ca016b7f09da5ef8b1d30d6c04a316b8c71
Ali-Alinejad <winford.op@gmail.com>
2024-09-05 16:04:19 +0330

    changes

--- onlineshop/src/sections/Global/cart.jsx
+++ onlineshop/src/sections/Global/cart.jsx
@@ -1,22 +1,56 @@
-
-import { Card, CardHeader, CardBody, Image } from "@nextui-org/react";
+import { Card, CardHeader, CardBody, Image, Button } from "@nextui-org/react";
+import { useState } from "react";
 
 export default function Cart({ product }) {
+  const [showDetails, setShowDetails] = useState(false);
+  const { rate } = product.rating;
+
+  const handleImageClick = () => {
+    setShowDetails(!showDetails);
+  };
+
   return (
-    <Card className="p-4 flex text-center items-center justify-center w-3/12 h-80 m-3 mt-20">
-      <CardBody className="overflow-visible py-2 m-3">
-        <Image
-          alt={product.title}
-          className="object-cover rounded-xl"
-          src={product.image}
-          width={270}
-        />
-      </CardBody>
-      <CardHeader className="pb-0 pt-2 px-4 flex-col justify-center">
-        <p className="text-tiny uppercase font-bold">{product.category}</p>
-        <small className="text-default-500">{product.description}</small>
-        <h4 className="font-bold text-large">${product.price}</h4>
-      </CardHeader>
-    </Card>
+    <div className="group relative bg-white">
+      <Card
+        className={`p-4 flex flex-col items-center justify-center w-full h-[450px] m-10 bg-white transition duration-300 ease-in-out transform hover:border-2 hover:ring-4 ring-yellow-400 border-yellow-400 group-hover:z-10 mx-auto ${
+          showDetails ? "opacity-0" : "opacity-100"
+        }`}
+      >
+        <div
+          className={`flex flex-col ${
+            showDetails ? "hidden" : "flex"
+          } items-center justify-center w-full h-full`}
+        >
+          <CardBody
+            className="overflow-visible py-2 bg-white transition duration-300 ease-in-out transform hover:scale-1.5"
+            onClick={handleImageClick}
+          >
+            <Image
+              alt={product.title}
+              className="object-contain rounded-xl h-60 w-full"
+              src={product.image}
+            />
+          </CardBody>
+          <CardHeader className="pb-0 pt-2 px-4 flex-col justify-center text-center bg-white">
+            <p className="text-tiny uppercase font-bold text-black">
+              {product.title}
+            </p>
+            <small className="text-default-500 text-black">
+              {rate} <span>⭐</span>
+            </small>
+            <Button color="danger" className="font-bold text-2xl m-5">
+              ${product.price}
+            </Button>
+          </CardHeader>
+        </div>
+      </Card>
+      {showDetails && (
+        <div className="absolute inset-0 flex items-center justify-center bg-white transition duration-300 ease-in-out">
+          <p className="text-tiny uppercase font-bold text-black">
+            {product.description}
+          </p>
+        </div>
+      )}
+    </div>
   );
 }
--- onlineshop/src/sections/MainPage/product/product.jsx
+++ onlineshop/src/sections/MainPage/product/product.jsx
@@ -1,6 +1,7 @@
 import  { useEffect, useState } from "react";
 import FakeApi from "../../Global/Api";
-import Cart from "../../Global/Cart";
+import Cart from "../../Global/cart";
+
 
 function Product() {
   const [products, setProducts] = useState([]);
@@ -13,11 +14,14 @@ function Product() {
     fetchData();
   }, []);
 
-  return (
-    <div className="flex flex-wrap justify-center">
-      {products.map((product) => (
-        <Cart key={product.id} product={product} />
-      ))}
+
+    return (
+    <div className="bg-white min-h-screen">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4  mx-20">
+        {products.map((product) => (
+          <Cart key={product.id} product={product} />
+        ))}
+      </div>
     </div>
   );
 }
