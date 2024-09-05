c7416afd2118ff2a73d022aba5757052d94ba6b6
Ali-Alinejad <winford.op@gmail.com>
2024-09-05 09:35:59 +0330

    start
    set

---
+++ onlineshop/src/sections/MainPage/MainPage.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@nextui-org/react"; //
+function MainPage() {
+  return (
+    <>
+      <Button color="primary" className="text-7xl p-10 text-slate-100">
+        sss
+      </Button>
+    </>
+  );
+}
+
+export default MainPage;
