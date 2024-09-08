950d09c75f1fdd0efbcc1c6de68eefdab1e13e2b
Ali-Alinejad <winford.op@gmail.com>
2024-09-08 10:43:22 +0330

    add keyframe for header

--- ElctroShop/src/Navbar/NavbarHeader.jsx
+++ ElctroShop/src/Navbar/NavbarHeader.jsx
@@ -14,86 +14,100 @@ import {
 } from "@nextui-org/react";
 import { useLocation } from "react-router-dom";
 import Logo from "../../public/images/logo.png"; // Ensure this path is correct
-
+import './navar.css'
 export default function NavbarHeader() {
   const location = useLocation();
-
+  
   return (
-    <Navbar isBordered height={"100px"}>
-      <NavbarContent className="justify-around">
-        <NavbarBrand className="">
-          <img src={Logo} alt="Logo" style={{ height: "150px" }} />
-          {/* Set height for better visibility */}
-        </NavbarBrand>
-        <NavbarContent className="sm:flex gap-20 ">
-          <NavbarItem>
-            <Link
-              className="cursor-pointer uppercase"
-              color={location.pathname === "/" ? "danger" : "foreground"}
-              href="/"
-              aria-current={location.pathname === "/" ? "page" : undefined}
-            >
-              Home
-            </Link>
-          </NavbarItem>
+    <>
+          <div id="Shadow"></div>
+      <Navbar isBordered isBlurred height={"100px"} className="z-10" id="bar">
+        <NavbarContent className="justify-around z-10">
+          <Link
+            className="cursor-pointer uppercase"
+            color={location.pathname === "/" ? "danger" : "foreground"}
+            href="/"
+            aria-current={location.pathname === "/" ? "page" : undefined}
+          >
+            <NavbarBrand className="">
+              <img
+                src={Logo}
+                alt="Logo"
+                className="cursor-pointer overflow-hidden h-40 w-36 mr-10 transition duration-300 hover:scale-125 "
+              />
+            </NavbarBrand>
+          </Link>
+          <NavbarContent className="sm:flex gap-20 ">
+            <NavbarItem>
+              <Link
+                className="cursor-pointer"
+                href="/order"
+                color={location.pathname === "/order" ? "danger" : "foreground"}
+              >
+                Category
+              </Link>
+            </NavbarItem>
 
-          <NavbarItem>
-            <Link
-              className="cursor-pointer"
-              href="/order"
-              color={location.pathname === "/order" ? "danger" : "foreground"}
-            >
-              Dashboard
-            </Link>
-          </NavbarItem>
+            <NavbarItem>
+              <Link
+                className="cursor-pointer"
+                href="/order"
+                color={location.pathname === "/order" ? "danger" : "foreground"}
+              >
+                Dashboard
+              </Link>
+            </NavbarItem>
+          </NavbarContent>
         </NavbarContent>
-      </NavbarContent>
 
-      <NavbarContent as="div" className="items-center" justify="end">
-        <Input
-          classNames={{
-            base: "max-w-full sm:max-w-[10rem] lg:max-w-[20rem] hover:w-[22rem] h-10",
-            mainWrapper: "h-full",
-            input: "text-small",
-            inputWrapper:
-              "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
-          }}
-          placeholder="Search"
-          size="sm"
-          type="search"
-        />
-        <Dropdown placement="bottom-end">
-          <DropdownTrigger>
-            <Avatar
-              isFocusable={true}
-              as="button"
-              className="transition-transform"
-              color="danger"
-              name=""
-              size="sm"
-              src="" // Add a profile image URL if needed
-            />
-          </DropdownTrigger>
-          <DropdownMenu aria-label="Profile Actions" variant="flat">
-            <DropdownItem key="profile" className="h-14 gap-2">
-              <li className="inline-flex gap-4">
-                <ul>
-                  <p className="font-semibold">Create Account</p>
-                </ul>
-                <ul>
-                  <Spinner size="sm" />
-                </ul>
-              </li>
-            </DropdownItem>
+        <NavbarContent as="div" className="items-center" justify="end">
+          <Input
+            classNames={{
+              base: "max-w-full sm:max-w-[10rem] lg:max-w-[20rem] hover:w-[22rem] h-10",
+              mainWrapper: "h-full",
+              input: "text-small",
+              inputWrapper:
+                "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
+            }}
+            placeholder="Search"
+            size="sm"
+            type="search"
+          />
+          <Dropdown placement="bottom-end">
+            <DropdownTrigger>
+              <Avatar
+                isFocusable={true}
+                as="button"
+                className="transition-transform"
+                color="danger"
+                name=""
+                size="sm"
+                src="" // Add a profile image URL if needed
+              />
+            </DropdownTrigger>
+            <DropdownMenu aria-label="Profile Actions" variant="flat">
+              <DropdownItem key="profile" className="h-14 gap-2">
+                <li className="inline-flex gap-4">
+                  <ul>
+                    <p className="font-semibold">Create Account</p>
+                  </ul>
+                  <ul>
+                    <Spinner size="sm" />
+                  </ul>
+                </li>
+              </DropdownItem>
 
-            <DropdownItem key="settings">Settings</DropdownItem>
-            <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
-            <DropdownItem key="logout" color="danger">
-              Log in
-            </DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
-      </NavbarContent>
-    </Navbar>
+              <DropdownItem key="settings">Settings</DropdownItem>
+              <DropdownItem key="help_and_feedback">
+                Help & Feedback
+              </DropdownItem>
+              <DropdownItem key="logout" color="danger">
+                Log in
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        </NavbarContent>
+      </Navbar>
+    </>
   );
 }
