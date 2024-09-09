1eacac69e1291a9c5db295c93cf297971b6e8461
Ali-Alinejad <winford.op@gmail.com>
2024-09-09 12:05:34 +0330

    end of intro

--- ElctroShop/src/Footer/Footer.jsx
+++ ElctroShop/src/Footer/Footer.jsx
@@ -19,25 +19,24 @@ const Footer = () => {
 
   const handleEmailSubmit = (e) => {
     e.preventDefault();
-    // Here you can add your email submission logic
     alert(`Email submitted: ${email}`);
     setEmail("");
   };
 
   return (
     <motion.footer
-      className="bg-white text-black mt-20 h-auto"
+      className="text-white mt-20 h-auto"
       initial={{ opacity: 0, y: 50 }}
       whileInView={{ opacity: 1, y: 0 }}
       viewport={{ once: true }}
       transition={{ duration: 0.5 }}
     >
-      <div className="mx-auto px-4 w-full h-auto bg-white  border-t-2 border-anim">
-        <div className="flex flex-col md:flex-row justify-between mt-10 p-14 bg-white">
+      <div className="mx-auto px-4 w-full h-auto bg-rose-900 ">
+        <div className="flex flex-col md:flex-row justify-between mt-10 p-14">
           <div className="mb-4 md:mb-0">
-            <h2 className="text-rose-600 text-3xl font-bold">ElectroShop</h2>
-            <p className="mt-2 text-gray-600">پیش قدم در تکنولوژی</p>
-            <p className="mt-2 text-gray-600">
+            <h2 className="text-rose-200 text-4xl font-bold">ElectroShop</h2>
+            <p className="mt-2 text-gray-400">پیش قدم در تکنولوژی</p>
+            <p className="mt-2 text-gray-400">
               ما بهترین محصولات الکترونیکی را با قیمت مناسب ارائه می‌دهیم.
             </p>
             <div className="flex items-center mt-4">
@@ -64,6 +63,21 @@ const Footer = () => {
               </li>
               <li>
                 <Button auto flat color="error" className="hover:bg-red-100">
+                  محصولات
+                </Button>
+              </li>
+              <li>
+                <Button auto flat color="error" className="hover:bg-red-100">
+                  وبلاگ
+                </Button>
+              </li>
+              <li>
+                <Button auto flat color="error" className="hover:bg-red-100">
+                  پشتیبانی
+                </Button>
+              </li>
+              <li>
+                <Button auto flat color="error" className="hover:bg-red-100">
                   تماس با ما
                 </Button>
               </li>
@@ -71,100 +85,99 @@ const Footer = () => {
           </div>
         </div>
 
-        {/* Email Subscription Section */}
-        <div className="flex flex-col items-center mt-8">
-          <h3 className="text-rose-600 font-bold mb-2">
-            برای دریافت آخرین اخبار ایمیل خود را وارد کنید:
-          </h3>
-          <form
-            onSubmit={handleEmailSubmit}
-            className="flex items-center space-x-2"
-          >
-            <Input
-              type="email"
-              value={email}
-              onChange={handleEmailChange}
-              placeholder="ایمیل خود را وارد کنید"
-              className="w-64"
-              required
-            />
-            <Button
-              type="submit"
-              color="danger"
-              auto
-              className="hover:bg-red-400"
+        <div className="flex flex-row justify-center border-t-1 ">
+          <div className="flex flex-col items-center mt-4 p-8 pr-0">
+            <h3 className="text-rose-50 font-bold mb-2 text-lg">
+              برای دریافت آخرین اخبار ایمیل خود را وارد کنید
+            </h3>
+
+            <form onSubmit={handleEmailSubmit} className="flex space-x-2">
+              <Input
+                type="email"
+                value={email}
+                onChange={handleEmailChange}
+                placeholder="ایمیل خود را وارد کنید"
+                className="w-64"
+                color=""
+                required
+              />
+              <Button
+                type="submit"
+                color="default"
+                auto
+                className="hover:bg-red-400"
+              >
+                تایید
+              </Button>
+            </form>
+          </div>
+
+          <div className="flex flex-row items-center m-10 gap-8 mt-14">
+            <motion.a
+              href="https://github.com/yourusername"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-rose-100 hover:text-red-200 transition duration-300"
+              whileHover={{ scale: 1.2 }}
+              whileTap={{ scale: 0.9 }}
             >
-              تایید
-            </Button>
-          </form>
+              <FaGithub size={30} />
+            </motion.a>
+            <motion.a
+              href="https://www.linkedin.com/in/yourusername"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-rose-100 hover:text-red-200 transition duration-300"
+              whileHover={{ scale: 1.2 }}
+              whileTap={{ scale: 0.9 }}
+            >
+              <FaLinkedin size={30} />
+            </motion.a>
+            <motion.a
+              href="https://www.instagram.com/yourusername"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-rose-100 hover:text-red-200 transition duration-300"
+              whileHover={{ scale: 1.2 }}
+              whileTap={{ scale: 0.9 }}
+            >
+              <FaInstagram size={30} />
+            </motion.a>
+            <motion.a
+              href="https://t.me/yourusername"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-rose-100 hover:text-red-200 transition duration-300"
+              whileHover={{ scale: 1.2 }}
+              whileTap={{ scale: 0.9 }}
+            >
+              <FaTelegram size={30} />
+            </motion.a>
+          </div>
         </div>
 
-        <div className="flex justify-center space-x-6 mt-8">
-          <motion.a
-            href="https://github.com/yourusername"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-rose-600 hover:text-red-400 transition duration-300"
-            whileHover={{ scale: 1.2 }}
-            whileTap={{ scale: 0.9 }}
-          >
-            <FaGithub size={30} />
-          </motion.a>
-          <motion.a
-            href="https://www.linkedin.com/in/yourusername"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-rose-600 hover:text-red-400 transition duration-300"
-            whileHover={{ scale: 1.2 }}
-            whileTap={{ scale: 0.9 }}
-          >
-            <FaLinkedin size={30} />
-          </motion.a>
-          <motion.a
-            href="https://www.instagram.com/yourusername"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-rose-600 hover:text-red-400 transition duration-300"
-            whileHover={{ scale: 1.2 }}
-            whileTap={{ scale: 0.9 }}
-          >
-            <FaInstagram size={30} />
-          </motion.a>
-          <motion.a
-            href="https://t.me/yourusername"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-rose-600 hover:text-red-400 transition duration-300"
-            whileHover={{ scale: 1.2 }}
-            whileTap={{ scale: 0.9 }}
-          >
-            <FaTelegram size={30} />
-          </motion.a>
-        </div>
-        <div className="mt-8 border-t border-gray-300 pt-4">
-          <div className="flex flex-col md:flex-row justify-between">
-            <div>
-              <h3 className="text-rose-600 font-bold mb-2">
-                اتاق خبر دیجی‌کالا
-              </h3>
-              <p className="text-gray-600">آخرین اخبار و مقالات</p>
-            </div>
-            <div>
-              <h3 className="text-rose-600 font-bold mb-2">فروش در دیجی‌کالا</h3>
-              <p className="text-gray-600">فرصت‌های شغلی</p>
-              <p className="text-gray-600">گزارش تخلف در دیجی‌کالا</p>
-            </div>
-            <div>
-              <h3 className="text-rose-600 font-bold mb-2">تماس با دیجی‌کالا</h3>
-              <p className="text-gray-600">درباره دیجی‌کالا</p>
-              <p className="text-gray-600">خدمات مشتریان</p>
-            </div>
-          </div>
-          <p className="text-center text-sm mt-4 text-gray-600">
+        <div className="mt-8 border-t border-gray-200 pt-4">
+          <p className="text-center text-sm mt-4 text-gray-200 pb-2">
             © {new Date().getFullYear()} ElectroShop. کلیه حقوق محفوظ است.
           </p>
         </div>
       </div>
+      <style>{`
+        @keyframes borderAnimation {
+          0% {
+            border-color: black;
+          }
+          50% {
+            border-color: red;
+          }
+          100% {
+            border-color: black;
+          }
+        }
+        .border-anim {
+          animation: borderAnimation 3s infinite;
+        }
+      `}</style>
     </motion.footer>
   );
 };
--- ElctroShop/src/Slides/Intro.jsx
+++ ElctroShop/src/Slides/Intro.jsx
@@ -1,16 +1,106 @@
-import { Image } from '@nextui-org/react';
-import intro from '../../public/images/thinkpad.png';
+import { Image } from "@nextui-org/react";
+import intro from "../../public/images/macBook1.png";
+import { Button } from "@nextui-org/react";
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+} from "@nextui-org/react";
 
 
 function Intro() {
   return (
     <>
-      <div className="w-[100%] h-[70vh] border-none">
-        
-        <div>
+      <div>
+        <Table
+          isStriped
+          aria-label=""
+          className="absolute w-[30%] left-28 top-60 max-lg:opacity-0
+          max-xl:opacity-0
+          2xl:opacity-100"
           
-</div>
-<Image src={intro} className='z-0' ></Image>
+          
+        >
+          <TableHeader>
+            <TableColumn>نام</TableColumn>
+            <TableColumn>مشخصات</TableColumn>
+          </TableHeader>
+          <TableBody>
+            <TableRow key="1">
+              <TableCell >برند</TableCell>
+              <TableCell>اپل</TableCell>
+            </TableRow>
+            <TableRow key="2">
+              <TableCell>رنگ بندی</TableCell>
+              <TableCell>⬜⬛🟨</TableCell>
+            </TableRow>
+            <TableRow  key="3">
+              <TableCell>سایز صفحه</TableCell>
+              <TableCell>14.5 اینچ</TableCell>
+            </TableRow>
+            <TableRow key="4">
+              <TableCell>گارانتی</TableCell>
+              <TableCell> دو سال</TableCell>
+            </TableRow>
+            <TableRow key="5">
+              <TableCell>ساخت</TableCell>
+              <TableCell>امریکا </TableCell>
+            </TableRow>
+            <TableRow key="6">
+              <TableCell>قطر</TableCell>
+              <TableCell>3 سانت</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </div>
+      <div
+        className="relative mt-8 mx-auto rounded-2xl flex justify-end flex-row h-[70vh] w-[90%]  shadow-xl
+      
+      max-sm:h-[40vh]
+      
+      
+      "
+      >
+        <h1
+          className="absolute max-lg:top-[85%]  max-lg:text-7xl 
+        
+        max-sm:text-5xl
+        max-sm:top-[74%]
+
+
+        lg:top-[80%]
+        lg:text-7xl
+        lg:left-[50%]
+        
+        opacity-50 font-thin left-4 top-12"
+        >
+          MAC BOOK PRO
+        </h1>
+        <Button
+          color="danger"
+          size="lg"
+          className=" absolute text-2xl  w-[560px] 
+        
+        max-sm:w-full
+        max-sm:p-4
+        max-sm:top-48
+        max-sm:left-0
+        max-sm:text-medium
+        max-lg:top-96
+      max-lg:left-20
+        lg:top-[80%]
+        2xl:left-5
+        2xl:top-[63%]
+        
+        font-mono z-10 font-bold left-10 top-90 shadow-lg bg-gradient-to-b 
+        from-rose-400 to-rose-700"
+        >
+          خرید اخرین مدل های مک بوک
+        </Button>
+         <Image src={intro} className="z-0"></Image>
       </div>
     </>
   );
